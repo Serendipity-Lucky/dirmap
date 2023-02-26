0cb73d9c5aab69d0c51fdd3733ef6c3e54a9d51b
Joker_ymz <109351276+Serendipity-Lucky@users.noreply.github.com>
2023-02-26 18:32:50 +0800

    Update dirmap.py

--- dirmap.py
+++ dirmap.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-'''
-@Author: xxlin
-@LastEditors: xxlin
-@Date: 2019-04-10 13:27:59
-@LastEditTime: 2019-05-01 17:57:11
-'''
-
 import os
 import sys
 
